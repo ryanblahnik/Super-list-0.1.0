e904d86701186f9a652af9818e6524a72b8c1637
Ryan Blahnik <testing@test.local>
2018-07-20 14:27:45 -0500

    Commit before walking away- starting styling

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// const myDB = require('myDatabaseInfo');
+//   for external storage
+
 $(document).ready(function() {
   // attach event listener to buttons(input)
   // create function stub for read/write/delete
@@ -8,21 +11,37 @@ $(document).ready(function() {
     let titleValue = $('.input-field-title').val();
     let contentValue = $('.input-field-body').val();    
     localStorage.setItem('titleValue', titleValue);
-    localStorage.setItem('contentValue', contentValue);    
+    localStorage.setItem('contentValue', contentValue);  
+    // myDB.put();  
   });
 
   $('.get-btn').on('click', function(event){
     // console.log(localStorage.getItem('hrext'));
     let titleValue = localStorage.getItem('titleValue');
     let contentValue = localStorage.getItem('contentValue');
+    // myDB.get();
     $('.debug').html(`<p>${titleValue} ${contentValue}</p>`);  
   });
 
   $('.delete-btn').on('click', function(event){
-    console.log(localStorage.removeItem('hrext'));
+    localStorage.removeItem('titleValue');
+    localStorage.removeItem('contentValue');
+
+    // if (!localStorage.getItem('titleValue') && !localStorage.getItem('contentValue') {
+    //   $('.debug').html(`<p>Items deleted</p>`); 
+    // } else {
+    //   $('.debug').html(`<p>HWHAP</p>`); 
+
+    // console.log(localStorage.removeItem('hrext'));
+
+    // TODO add in a confirm
+        // Window.confirm();
+        // result = window.confirm(message)
+        //  if statement around that line
+    // throw up a confirm window
+    // capture result
+    // test boolean to delete or not
+    
   });
 
 });
-
-
-
