687527cd3a4a8eeeedfa9f0fb795b69846f91999
Ryan Blahnik <testing@test.local>
2018-07-20 13:28:31 -0500

    Initial write/read/delete

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ $(document).ready(function() {
   // $().on('click', function(){
 
   // });
-  $().on('click', function(){
-
+  $('.set-btn').on('click', function(event){
+    localStorage.setItem('hrext', "three is the best");
   });
-  
+
+  $('.get-btn').on('click', function(event){
+    console.log(localStorage.getItem('hrext'));
+  });
+
+  $('.delete-btn').on('click', function(event){
+    console.log(localStorage.removeItem('hrext'));
+  });
+
 });
+
+
+
