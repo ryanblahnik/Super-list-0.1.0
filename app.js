e8a5698badbfde07f3e79b2deee40b0d7918feb9
Ryan Blahnik <testing@test.local>
2018-07-20 12:45:49 -0500

    Planning and initial outlines

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  // attach event listener to buttons(input)
+  // create function stub for read/write/delete
+  // $().on('click', function(){
+
+  // });
+  $().on('click', function(){
+
+  });
+  
+});
