1554807b436be7774adce6d7de2b94cafcc40215
Ryan Blahnik <ryanblahnik@gmail.com>
2018-07-26 18:47:10 -0500

    Working with lists

--- app.js
+++ app.js
@@ -2,35 +2,48 @@
 //   for external storage
 
 $(document).ready(function() {
+  let newList = [];
+  localStorage.setItem('newList', newList);
+  // localStorage.setItem('newList', []);
+  // let i = 0;
   // attach event listener to buttons(input)
   // create function stub for read/write/delete
   // $().on('click', function(){
 
   // });
-  $('.set-btn').on('click', function(event){
-    let titleValue = $('.input-field-title').val();
-    let contentValue = $('.input-field-body').val();    
-    localStorage.setItem('titleValue', titleValue);
-    localStorage.setItem('contentValue', contentValue);  
-    // myDB.put();  
+  $('body').on('click', '.add', function(event){
+    let nextItem = $('.addition').val();
+    newList.push(nextItem);
+    // let contentValue = $('.input-field-body').val();
+    localStorage.setItem('newList', newList);
+    // i += 1;
+    // localStorage.setItem('contentValue', contentValue);
+    // existing = $('.progress').html();
+
+    $('.progress').append(nextItem + '<br>');
+
+
+
+    // $('.progress').html(existing + '\n' + nextItem);
+    // myDB.put();
   });
 
-  $('.get-btn').on('click', function(event){
+  $('body').on('click', function(event){
     // console.log(localStorage.getItem('hrext'));
     let titleValue = localStorage.getItem('titleValue');
-    let contentValue = localStorage.getItem('contentValue');
+    // let contentValue = localStorage.getItem('contentValue');
     // myDB.get();
-    $('.debug').html(`<p>${titleValue} ${contentValue}</p>`);  
+    // $('.debug').html(`<p>${titleValue} ${contentValue}</p>`);
   });
 
   $('.delete-btn').on('click', function(event){
     localStorage.removeItem('titleValue');
-    localStorage.removeItem('contentValue');
+    // localStorage.removeItem('contentValue');
 
     // if (!localStorage.getItem('titleValue') && !localStorage.getItem('contentValue') {
-    //   $('.debug').html(`<p>Items deleted</p>`); 
+    //   $('.debug').html(`<p>Items deleted</p>`);
     // } else {
-    //   $('.debug').html(`<p>HWHAP</p>`); 
+    //   $('.debug').html(`<p>HWHAP</p>`);
 
     // console.log(localStorage.removeItem('hrext'));
 
@@ -41,7 +54,7 @@ $(document).ready(function() {
     // throw up a confirm window
     // capture result
     // test boolean to delete or not
-    
+
   });
 
 });
